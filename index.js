fc52378a34d0d2e894b05144cb67aec412f32fd5
Kristian Estrada <kristianestrada209@gmail.com>
2018-09-05 14:32:22 -0700

    Added break length feature

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+import ReactDOM from 'react-dom'
+import React from 'React'
+
+
+class BreakLength extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div className='breakLabel'>
+        <div id='break-label' value={this.props.breakLength}>Break Length</div>
+        <button id='break-increment'className='icono-caretUpCircle' onClick={this.props.incrementBreak}>UP</button>
+        <span id='break-length'value='5'>{this.props.breakLength}</span>
+        <button id='break-decrement' className='icono-caretDownCircle' onClick={this.props.decrementBreak}>DOWN</button>
+      </div>
+    )
+  }
+}
+
 class SessionLength extends React.Component{
   constructor(props){
     super(props);
