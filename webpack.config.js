da4d24411edea5ef89cf29a7870b1963ed8cf7bf
Kristian Estrada <kristianestrada209@gmail.com>
2018-09-06 14:28:22 -0700

    Added node babel node modules, webpack modules and fixed dom target element

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
